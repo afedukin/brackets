edba69283c7d1d05c0959cd6dfef30ccba105072
afedukin <fedukin.aa@yandex.ru>
2024-09-03 20:16:30 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let open = [];
+  let close = [];
+  let stack = [];
+  let pair = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+
+  for (let k = 0; k < open.length; k++){
+    pair[close[k]] = open[k];
+     }
+    // console.log(open)
+    // console.log(pair)
+ 
+
+  for (let j = 0; j < str.length; j++) {
+    let curr = str[j];
+    if (open.includes(curr)) {
+      stack.push(curr);
+    } else {
+
+      let top = stack[stack.length - 1];
+      if (pair[curr] == top) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
